22301c4432bc73fed6d4548a35f9330b7ee4937a
Linas <linasnorkus92@gmail.com>
2023-02-28 16:15:37 -0500

    works consistently, both tests pass

--- test_ls.py
+++ test_ls.py
@@ -6,15 +6,19 @@ import subprocess
 from pathlib import Path
 
 
-def test_list_empty_folder():# should pass (once)
-    os.mkdir("/tmp/testfolder")
-    result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
-    assert not result.stdout
-    shutil.rmtree("/tmp/testfolder")
+def test_list_empty_folder():
+    try:
+        os.mkdir("/tmp/testfolder")
+        result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
+        assert not result.stdout, "Listing an empty folder did not return expected result"
+    finally:
+        shutil.rmtree("/tmp/testfolder")
 
-def test_simple_ls():#s hould fail
-    os.mkdir("/tmp/testfolder")
-    Path("/tmp/testfolder/first.txt").touch()
-    result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
-    assert not result.stdout
-    shutil.rmtree("/tmp/testfolder")
+def test_simple_ls():
+    try:
+        os.mkdir("/tmp/testfolder")
+        Path("/tmp/testfolder/first.txt").touch()
+        result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
+        assert "first.txt" in str(result.stdout), "Listing a folder with one file did not return expected result"
+    finally:
+        shutil.rmtree("/tmp/testfolder")
