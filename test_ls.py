17ad7b583450d71425bfbe050712a9a81c049c2d
Linas <linasnorkus92@gmail.com>
2023-02-28 16:22:26 -0500

    polished and left for tomorrow

--- test_ls.py
+++ test_ls.py
@@ -6,19 +6,23 @@ import subprocess
 from pathlib import Path
 
 
-def test_list_empty_folder():
-    try:
-        os.mkdir("/tmp/testfolder")
-        result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
-        assert not result.stdout, "Listing an empty folder did not return expected result"
-    finally:
-        shutil.rmtree("/tmp/testfolder")
+class TestClass(object):
+    @staticmethod
+    def test_list_empty_folder():
+        try:
+            os.mkdir("/tmp/testfolder")
+            result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
+            assert not result.stdout, "Listing an empty folder did not return expected result"
+        finally:
+            shutil.rmtree("/tmp/testfolder")
 
-def test_simple_ls():
-    try:
-        os.mkdir("/tmp/testfolder")
-        Path("/tmp/testfolder/first.txt").touch()
-        result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
-        assert "first.txt" in str(result.stdout), "Listing a folder with one file did not return expected result"
-    finally:
-        shutil.rmtree("/tmp/testfolder")
+    @staticmethod
+    def test_simple_ls():
+        try:
+            os.mkdir("/tmp/testfolder")
+            Path("/tmp/testfolder/first.txt").touch()
+            result = subprocess.run(["ls", "/tmp/testfolder"], stdout=subprocess.PIPE)
+            print(f"this is the result [{result}]")
+            assert "first.txt" in str(result.stdout), "Listing a folder with one file did not return expected result"
+        finally:
+            shutil.rmtree("/tmp/testfolder")
