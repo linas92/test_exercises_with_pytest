f9870ee3e58afd47ccf30949961eddb56fff6fc7
Linas <linasnorkus92@gmail.com>
2023-03-01 09:06:30 -0500

    should fail test added

--- test_ls.py
+++ test_ls.py
@@ -1,5 +1,6 @@
-# list directory contents ( ls ) testing
+# list directory contents ( ls ) testing & dir 
 import os
+import sys
 import shutil
 import pytest
 import subprocess
@@ -16,6 +17,7 @@ class TestClass(object):
         finally:
             shutil.rmtree("/tmp/testfolder")
 
+
     @staticmethod
     def test_simple_ls():
         try:
@@ -26,6 +28,7 @@ class TestClass(object):
             assert "first.txt" in str(result.stdout), "Listing a folder with one file did not return expected result"
         finally:
             shutil.rmtree("/tmp/testfolder")
+
 
     @staticmethod
     def test_list_mutiple_files():
@@ -39,6 +42,7 @@ class TestClass(object):
             assert "second.doc" in str(result.stdout), "Listing a folder with multiple files did not return expected result"
         finally:
             shutil.rmtree("/tmp/testfolder")
+
 
     @staticmethod
     def test_hidden_files():
@@ -54,6 +58,7 @@ class TestClass(object):
         finally:
             shutil.rmtree("/tmp/testfolder")
 
+
     @staticmethod
     def test_list_hidden_files():
         try:
@@ -67,3 +72,63 @@ class TestClass(object):
             assert ".hidden_file" in str(result.stdout), "ls listed hidden file when it shouldn't have"
         finally:
             shutil.rmtree("/tmp/testfolder")
+            
+
+    @staticmethod
+    def test_list_hidden_files():
+        try:
+            os.mkdir("/tmp/testfolder")
+            Path("/tmp/testfolder/first.txt").touch()
+            Path("/tmp/testfolder/second.doc").touch()
+            Path("/tmp/testfolder/.hidden_file").touch()
+            result = subprocess.run(["ls", "-a", "/tmp/testfolder"], stdout=subprocess.PIPE)
+            print(f"this is the result [{result}]")
+            assert "first.txt" in str(result.stdout), "Listing a folder with a hidden file did not return expected result"
+            assert ".hidden_file" in str(result.stdout), "ls listed hidden file when it shouldn't have"
+        finally:
+            shutil.rmtree("/tmp/testfolder")
+            
+
+    @staticmethod
+    @pytest.mark.skipif(sys.platform.startswith("win"), reason="Skipping non-windows test")
+    def test_list_hidden_files_test_with_mark():
+        try:
+            os.mkdir("/tmp/testfolder")
+            Path("/tmp/testfolder/first.txt").touch()
+            Path("/tmp/testfolder/second.doc").touch()
+            Path("/tmp/testfolder/.hidden_file").touch()
+            result = subprocess.run(["ls", "-a", "/tmp/testfolder"], stdout=subprocess.PIPE)
+            print(f"this is the result [{result}]")
+            assert "first.txt" in str(result.stdout), "Listing a folder with a hidden file did not return expected result"
+            assert ".hidden_file" in str(result.stdout), "ls listed hidden file when it shouldn't have"
+        finally:
+            shutil.rmtree("/tmp/testfolder")
+
+   
+    @staticmethod
+    def test_dir_windows_only():
+        if not sys.platform.startswith("win"):
+            pytest.skip("Skipping windows-only test")
+        try:
+            os.mkdir("c\testfolder")
+            Path("c\testfolder\first.txt").touch()
+            result = subprocess.run(["dir", "c\testfolder"], stdout=subprocess.PIPE)
+            print("Result: [{}]".format(result))
+            assert "first.txt" in str(result.stdout), "Listing a folder ON WINDOWS with one file did not return as expected"
+        finally:
+            shutil.rmtree("c\testfolder")
+
+
+    @staticmethod
+    @pytest.mark.skipif(not sys.platform.startswith("win"), reason="Skipping windows-only test")# same test with "pytest.mark."
+    def test_dir_windows_only_test_with_mark():
+        try:
+            os.mkdir("c\testfolder")
+            Path("c\testfolder\first.txt").touch()
+            result = subprocess.run(["dir", "c\testfolder"], stdout=subprocess.PIPE)
+            print("Result: [{}]".format(result))
+            assert "first.txt" in str(result.stdout), "Listing a folder ON WINDOWS with one file did not return as expected"
+        finally:
+            shutil.rmtree("c\testfolder")
+
+
